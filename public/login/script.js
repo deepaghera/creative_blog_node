90b1866c6cffebb0e47502de6a81ea5d5712e54b
DESKTOP-0JMBICS\kjkkjjjk <deepaghera001@gmail.com>
2020-12-25 12:21:26 +0530

    database added

--- public/login/script.js
+++ public/login/script.js
@@ -3,3 +3,19 @@ document.querySelector('.img-btn').addEventListener('click', function()
 		document.querySelector('.cont').classList.toggle('s-signup')
 	}
 );
+
+// NOTE: conform password validation
+document.querySelector('#r_btn').addEventListener('click', function(event)
+{
+	//event.preventDefault()
+	var pass = document.querySelector('#r_pass').value;
+	var c_pass = document.querySelector('#r_cpass').value;
+	if (pass === c_pass) {
+		event.currentTarget
+	}
+	else {
+		event.preventDefault()
+		document.querySelector('.p_err').style.visibility = 'visible'
+	}
+	console.log(pass,c_pass);
+});
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const path = require("path");
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended : true}));
+
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password: "",
+    database:"creative_blog"
+})
+
+con.connect(function(err) {
+    if(err) throw err;
+    console.log("connected");
+})
 
 app.get('/',function (req,res)  {
     // console.log(__dirname);
@@ -17,11 +30,26 @@ app.get('/login',function(req,res) {
     // res.send("login paage");
 })
 
-app.post('/loginsubmit',function(req,res) {
+app.post('/login_post',function(req,res) {
     // console.log(req.params);
     console.log(req.body);
     res.send("Post succesfuly")
 })
+
+app.post('/Register_post',function(req,res) {
+    // console.log(req.params);
+    const { register_name , register_email,register_pass} = req.body;
+    var data={NAME:register_name,Email:register_email,PASSWORD:register_pass};
+
+    var sql = "INSERT INTO USERS SET ?"; 
+    con.query(sql,data,function(err,result) {
+        if(err) throw err;
+        console.log("1 record inserted");
+    })   
+    console.log(req.body);
+    res.send("Post succesfuly")
+})
+
 app.get('/:any' , function(req,res) {
     console.log(req.params.any);
     res.send("Not listed");
